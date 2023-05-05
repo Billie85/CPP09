01e4a87c2a61b6c1b3b2a2862ea2183cd55515d7
Angelinne <kayumi85@hotmail.com>
2023-05-06 07:21:15 +0900

    new

--- ex00/test.cpp
+++ ex00/test.cpp
@@ -1,58 +1,13 @@
-#include <fstream>
 #include <iostream>
-#include <map>
-#include <string>
-#include <cstdlib>
+#include <iomanip>
+#include <cfloat>
+#include <cmath>
 
-void removeSpaces(std::string& str) {
-    std::string::iterator end_pos = std::remove(str.begin(), str.end(), ' ');
-    str.erase(end_pos, str.end());
-}
+int main()
+{
+  std::cout << std::setprecision(DBL_DIG);
+  std::cout << DBL_MIN << '\n';
 
-bool FindMinus(std::string& str) {
-    return str.find('-') == std::string::npos;
-}
-
-bool CheckDecimals(std::string& str) {
-    return str.find('.') != std::string::npos;
-}
-
-void inputData(std::ifstream &file, std::map<std::string, double>& CsvArray) {
-    if (!file.is_open()) {
-        std::cout << "Error: could not open file." << std::endl;
-        return;
-    }
-
-    std::string str;
-    std::string data;
-    int numInt = 0;
-    double numDouble = 0;
-    while (getline(file, str, '|')) 
-    {
-        getline(file, data, '\n');
-        removeSpaces(str);
-        if (!FindMinus(data)) {//マイナスがあったら。
-            std::cout << "Error: not a positive number." << std::endl;
-            continue;
-        }
-        std::map<std::string, double>::iterator find = CsvArray.find(str);
-        if (!CheckDecimals(data) && find != CsvArray.end()) 
-        {//少数じゃない。
-            printf("Int\n");
-            numInt = atoi(data.c_str());
-            std::cout << str << " => " << CsvArray[str] << " = " << CsvArray[str] * numInt << std::endl;
-        }
-        if (CheckDecimals(data) && find != CsvArray.end()) 
-        {//少数。
-            printf("double\n");
-            numDouble = atof(data.c_str());
-            std::cout << str << " => " << CsvArray[str] << " = " << CsvArray[str] * numDouble << std::endl;
-        }
-        else 
-        {
-            std::cout << "Error: bad input => " << str << std::endl;
-            continue;
-        }
-    }
-    file.close();
+  // 以下の式と等価
+  std::cout << std::pow(FLT_RADIX, DBL_MIN_EXP - 1) << '\n';
 }
