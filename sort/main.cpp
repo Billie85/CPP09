64715900ba35aef5febebf650391aa838cb9e960
Kimura Ayumi <kayumi@c1r14s1.42tokyo.jp>
2023-10-03 22:08:09 +0900

    new

--- sort/main.cpp
+++ sort/main.cpp
@@ -1,81 +1,60 @@
 #include <iostream>
 #include <vector>
 #include <algorithm>
-#include <utility>
 
-// ペアの大きい方を前にする関数
-void arrangePairs(std::pair<int, int>& pair) {
-    if (pair.first < pair.second) {
-        std::swap(pair.first, pair.second);
-    }
+// 二分探索を使用してソートされたベクトルに要素を挿入する関数
+template <typename T>
+void binaryInsert(std::vector<T>& vec, const T& element) {
+    typename std::vector<T>::iterator it = std::lower_bound(vec.begin(), vec.end(), element);
+    vec.insert(it, element);
 }
 
-// ペアを作成する関数
-std::vector<std::pair<int, int> > createPairs(const std::vector<int>& input) {
-    std::vector<std::pair<int, int> > pairs;
-    for (size_t i = 0; i < input.size(); i += 2) {
-        if (i + 1 < input.size()) {
-            std::pair<int, int> current_pair = std::make_pair(input[i], input[i + 1]);
-            arrangePairs(current_pair); // 大きい方を前にする
-            pairs.push_back(current_pair);
+// マージ挿入ソートアルゴリズムを実装する関数
+template <typename T>
+void mergeInsertionSort(std::vector<T>& arr) {
+    int n = arr.size();
+    
+    if (n <= 1)
+        return;
+
+    std::vector<T> largerElements;  // 大きな要素を保持するベクトル
+    std::vector<T> smallerElements; // 小さな要素を保持するベクトル
+    
+    // ステップ1: 要素をペアにグループ化し、大きな要素を見つける
+    for (int i = 0; i < n - 1; i += 2) {
+        if (arr[i] < arr[i + 1]) {
+            smallerElements.push_back(arr[i]);
+            largerElements.push_back(arr[i + 1]);
         } else {
-            pairs.push_back(std::make_pair(input[i], input[i]));
+            smallerElements.push_back(arr[i + 1]);
+            largerElements.push_back(arr[i]);
         }
     }
-    return pairs;
-}
 
-// ペアを大きい要素を基準に比較してソートする関数
-bool comparePairs(const std::pair<int, int>& a, const std::pair<int, int>& b) {
-    return std::max(a.first, a.second) < std::max(b.first, b.second);
-}
+    // ステップ3: 大きな要素を再帰的にソート
+    mergeInsertionSort(largerElements);
 
-// 再帰的にペアをソートする関数
-void recursiveSort(std::vector<std::pair<int, int> >& pairs, int left, int right) {
-    if (left < right) {
-        int mid = (left + right) / 2;
-        recursiveSort(pairs, left, mid);
-        recursiveSort(pairs, mid + 1, right);
-        
-        // 一時的なベクターを作成してソートし、ソートされたペアをコピー
-        std::vector<std::pair<int, int> > temp;
-        for (int i = left; i <= right; ++i) {
-            temp.push_back(pairs[i]);
-        }
-        
-        std::sort(temp.begin(), temp.end(), comparePairs);
-        for (int i = left; i <= right; ++i) {
-            pairs[i] = temp[i - left];
-        }
+    // ステップ4: 最小の要素とペアにされた要素の先頭に挿入
+    arr.clear();
+    arr.push_back(smallerElements[0]);
+    
+    // ステップ5: 特別に選ばれた挿入順序を使用して残りの要素を挿入
+    for (int i = 1; i < n - 1; ++i) {
+        if (i % 2 == 1)
+            binaryInsert(arr, largerElements[i / 2]);
+        else
+            arr.push_back(smallerElements[i / 2]);
     }
 }
 
 int main() {
-    std::vector<int> input;
-    input.push_back(3);
-    input.push_back(9);
-    input.push_back(2);
-    input.push_back(5);
-    input.push_back(7);
-    input.push_back(8);
-    input.push_back(1);
-    input.push_back(6);
+    std::vector<int> arr = {5, 3, 7, 1, 9, 2, 8, 4, 6};
+    
+    mergeInsertionSort(arr);
 
-    // ペアを作成
-    std::vector<std::pair<int, int> > pairs = createPairs(input);
-
-    std::cout << "Pairs: ";
-    for (size_t i = 0; i < pairs.size(); ++i) {
-        std::cout << "(" << pairs[i].first << ", " << pairs[i].second << ") ";
-    }
-    std::cout << std::endl;
-
-    // 再帰的にペアをソート
-    recursiveSort(pairs, 0, pairs.size() - 1);
-
-    std::cout << "Sorted pairs: ";
-    for (size_t i = 0; i < pairs.size(); ++i) {
-        std::cout << "(" << pairs[i].first << ", " << pairs[i].second << ") ";
+    std::cout << "ソートされた配列: ";
+    for (int i = 0; i < arr.size(); ++i) {
+        std::cout << arr[i] << " ";
     }
     std::cout << std::endl;
 
