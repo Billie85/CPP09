3cfa4d5acbd39c114fc961e68560656caafd18e9
Kimura Ayumi <kayumi@c1r27s9.42tokyo.jp>
2023-10-04 22:06:23 +0900

    new

--- sort/main.cpp
+++ sort/main.cpp
@@ -1,62 +1,40 @@
 #include <iostream>
 #include <vector>
-#include <algorithm>
+#include <cstdlib>
 
-// 二分探索を使用してソートされたベクトルに要素を挿入する関数
-template <typename T>
-void binaryInsert(std::vector<T>& vec, const T& element) {
-    typename std::vector<T>::iterator it = std::lower_bound(vec.begin(), vec.end(), element);
-    vec.insert(it, element);
-}
+int main(int argc, char *argv[]) {
+    // コマンドライン引数の数が正確に1つでない場合はエラーメッセージを表示して終了
+    if (argc != 2) {
+        std::cout << "Argument Error: Please provide one argument." << std::endl;
+        return 1;
+    }
+    std::string arg = argv[1];
 
-// マージ挿入ソートアルゴリズムを実装する関数
-template <typename T>
-void mergeInsertionSort(std::vector<T>& arr) {
-    int n = arr.size();
-    
-    if (n <= 1)
-        return;
-
-    std::vector<T> largerElements;  // 大きな要素を保持するベクトル
-    std::vector<T> smallerElements; // 小さな要素を保持するベクトル
-    
-    // ステップ1: 要素をペアにグループ化し、大きな要素を見つける
-    for (int i = 0; i < n - 1; i += 2) {
-        if (arr[i] < arr[i + 1]) {
-            smallerElements.push_back(arr[i]);
-            largerElements.push_back(arr[i + 1]);
-        } else {
-            smallerElements.push_back(arr[i + 1]);
-            largerElements.push_back(arr[i]);
+    // 文字列が数字のみで構成されていることを確認
+    for (size_t i = 0; i < arg.size(); i++) {
+        if (!isdigit(arg[i])) {
+            std::cout << "Argument Error: Only numbers are allowed." << std::endl;
+            return 1;
         }
     }
+    std::vector<int> vectorArray;
 
-    // ステップ3: 大きな要素を再帰的にソート
-    mergeInsertionSort(largerElements);
+    // 文字列を一文字ずつ処理して重複を確認
+    for (size_t i = 0; i < arg.size(); i++) {
+        int digit = arg[i] - '0'; // 文字を数字に変換
 
-    // ステップ4: 最小の要素とペアにされた要素の先頭に挿入
-    arr.clear();
-    arr.push_back(smallerElements[0]);
-    
-    // ステップ5: 特別に選ばれた挿入順序を使用して残りの要素を挿入
-    for (int i = 1; i < n - 1; ++i) {
-        if (i % 2 == 1)
-            binaryInsert(arr, largerElements[i / 2]);
-        else
-            arr.push_back(smallerElements[i / 2]);
+        // 重複する数字がベクターに存在する場合はエラーメッセージを表示して終了
+        for (size_t j = 0; j < vectorArray.size(); j++) {
+            if (vectorArray[j] == digit) {
+                std::cout << "Argument Error: Duplicate numbers are not allowed." << std::endl;
+                return 1;
+            }
+        }
+        vectorArray.push_back(digit);
     }
-}
+    int result = vectorArray.back();
+    std::cout << "double: " << result * 2 << std::endl;
 
-int main() {
-    std::vector<int> arr = {5, 3, 7, 1, 9, 2, 8, 4, 6};
-    
-    mergeInsertionSort(arr);
-
-    std::cout << "ソートされた配列: ";
-    for (int i = 0; i < arr.size(); ++i) {
-        std::cout << arr[i] << " ";
-    }
-    std::cout << std::endl;
 
     return 0;
 }
