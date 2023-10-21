eb31b831bff06537014e43befd4363e9fb91771a
Kimura Ayumi <kayumi@c1r18s8.42tokyo.jp>
2023-10-21 17:58:24 +0900

    Error

--- ex02/PmergeMe.hpp
+++ ex02/PmergeMe.hpp
@@ -1,20 +1,3 @@
-//version:1.0.0
-
-// #ifndef DEBUG_H
-// #define DEBUG_H
-#include <iostream>
-#include <unistd.h>
-#include <sys/time.h>
-// #define TEST(msg) std::cout<<"[("<< __FILE__<<"/"<<__LINE__<<") "<<__func__<<" ]"<<msg<<std::endl;
-// #define TEST_(msg) std::cout<<"---------------------------------------------[("<< __FILE__<<"/"<<__LINE__<<") "<<__func__<<" ]"<<msg<<std::endl;
-// #define TESTv(msg, i) std::cout<<"[("<< __FILE__<<"/"<<__LINE__<<") "<<__func__<<" ]"<<msg<<":"<<i<<std::endl;
-// #define T std::cout<<"[("<< __FILE__<<"/"<<__LINE__<<") "<<__func__<<" ]"<<std::endl;
-// #define T_ std::cout<<"---------------------------------------------[("<< __FILE__<<"/"<<__LINE__<<") "<<__func__<<" ]"<<std::endl;
-// #define Tv(i) std::cout<<"[("<< __FILE__<<"/"<<__LINE__<<") "<<__func__<<" ]"<<#i<<":"<<i<<std::endl;
-// #define FOR(x,j) {std::cout<<"[("<< __FILE__<<"/"<<__LINE__<<") "<<__func__<<" ]"<<#x<<":";for(size_t i = 0; x + i != j; i++){std::cout<<x[i]<<",";}std::cout<<std::endl;}
-// #define TAKE(i,j) {static size_t test_arg = 0; if(!(test_arg % j)){i} test_arg++;}
-// #define STOP {char c; read(STDIN_FILENO, &c, sizeof(char));}
-
 #ifndef PMERGE_HPP
 #define PMERGE_HPP
 
@@ -25,6 +8,9 @@
 #include <list>
 #include <stdlib.h>
 #include <iterator>
+#include <iostream>
+#include <unistd.h>
+#include <sys/time.h>
 
 class Unit
 {
@@ -42,9 +28,7 @@ public:
 };
 
 
-
-
-
+//////////////////////////////////////////////// std::list operator[] ////////////////////////////////////////////////////////////
 template <typename T>
 class MyList : public std::list<T>{
 
@@ -65,41 +49,180 @@ class MyList : public std::list<T>{
 	//listの中にoperator[]が無いため、std::listはクラスだからそれを継承してあげてから、operator[]の実装をしてあげる。classを使わないとできない。
 	T& operator[](size_t n)
 	{
-		
 		typename std::list<T>::iterator it = this->std::list<T>::begin();
 	    std::advance(it, n);
     	return *it;
 	}
 
-
-
 	//list iteratorの中にoperator[]が無いため、operator[]の実装をしてあげる。classを使わないとできない。
 	class iterator : public std::list<T>::iterator
 	{
 		//constructor
-		private:
-			iterator():std::list<T>::iterator(){}
 		public:
-			iterator(typename std::list<T>::iterator&v):std::list<T>::iterator(v){}
-			iterator(iterator &v):std::list<T>::iterator(v){}
-			iterator &operator=(const iterator &other){ static_cast<typename std::list<T>::iterator>(*this) = static_cast<typename std::list<T>::iterator>(other); return *this;}
-		T& operator[](size_t n)
-		{
-			typename std::list<T>::iterator it = *this;
-	    	std::advance(it, n);
-    		return *it;
-		}
+			iterator():std::list<T>::iterator(){}
+
+			iterator(typename std::list<T>::iterator &it):std::list<T>::iterator(it){}
+
+			iterator(iterator& v):std::list<T>::iterator(v){}
+
+			iterator& operator=(const iterator& other) {
+            std::list<T>::iterator::operator=(other);
+            return *this;
+			}
+
+			T& operator[](size_t n) {
+				iterator it = *this;
+				std::advance(it, n);
+				return *it;
+			}
 	};
 };
 
 
+//////////////////////////////////////////// Operator ////////////////////////////////////////////////////////////////
+
 bool operator>(const Unit &a, const Unit &b);
 bool operator<(const Unit &a, const Unit &b);
 std::ostream &operator<<(std::ostream &os, const Unit &other);
-// void ListSort(std::list<Unit> &src);
-void VectorSort(std::vector<Unit> &src);
-void divideIntoThree(std::vector<Unit> &src, std::vector<Unit> \
-	&destFront, std::vector<Unit> &destBack, std::vector<Unit> &remainder);
-void SwapBigger(std::vector<Unit> &Front, std::vector<Unit> &Back);
-void openPair(std::vector<Unit> &Src, std::vector<Unit> &Front, std::vector<Unit> &Back);
+
+/////////////////////////////////////////// Templat Function /////////////////////////////////////////////////////////////////
+
+template <typename Container>
+void Copy(Container &src, Container &dest){
+	
+	size_t len = src.size();
+	dest.clear();
+	dest.reserve(len);
+	for (size_t i = 0; i < len; i++){
+		Unit buff = src[i].MainNumber;
+		buff.ChildIndex = i;
+		buff.prevOfset = i;
+		buff.Haspair = false;
+		dest.push_back(buff);
+	}
+}
+
+template <typename Container>
+void SetHaspair(Container &target){
+	for (size_t i = 0; i < target.size(); i++)
+	{
+		target[i].Haspair = true;
+	}
+}
+
+template <typename Container>
+typename Container::iterator BinarySearch(Container &target, Unit &searchNumber, size_t len)
+{
+	//std::listの場合ここのイテレータでoperator[]が必要になる。
+	typename Container::iterator left = target.begin();
+
+	while (len > 1) {
+		if (searchNumber < left[len/2]){
+			len /= 2;
+		}else{
+			left += len / 2;
+			len = len / 2 + len % 2;
+		}
+	}
+	if (searchNumber < *left || !len){
+		return(left);
+	}else{
+		return(left + 1);
+	}
+}
+
+// J(0) = 0
+// J(1) = 1
+// J(n) = J(n-1) + 2 * J(n-2) （n > 1）
+
+template <typename Container>
+void openPair(Container &Src, Container &Front, Container &Back){
+	size_t i = 0;
+	
+	//std::list<Unit> ここのlistでoperator[]が必要になる。
+	if (Src.size()){
+		size_t n1 = 1;
+		size_t n2 = 1;
+		size_t open = 1;
+		while(open){
+			for (; open ; i--){
+				while (Src[i].Haspair && open){
+					size_t ChildIndex = Src[i].ChildIndex;
+					Src[i] = Front[Src[i].prevOfset];
+					Src.insert(BinarySearch(Src, Back[ChildIndex], i),Back[ChildIndex]);
+					open--;
+				}
+				if (!i)
+					break;
+			}
+			size_t hoge = n1 + 2 * n2;
+			n2 = n1;
+			n1 = hoge;
+			hoge = n1 - n2;
+			size_t len = Src.size();
+			for (; i < len && open < hoge; i++){
+				if (Src[i].Haspair){
+					open++;
+					if (open >= hoge){
+						break;
+					}
+				}
+			}
+		}
+	}
+}
+
+template <typename Container>
+void SwapBigger(Container &Front, Container &Back)
+{
+	size_t len = Front.size();
+	for (size_t i = 0; i < len; i++)
+	{
+		if (Front[i] < Back[i])
+		{
+			Unit buff = Front[i];
+			Front[i] = Back[i];
+			Back[i] = buff;
+		}
+	}
+}
+
+template <typename Container>
+void divideIntoThree(Container &src, Container &destFront, Container &destBack, Container &remainder){
+		size_t len = src.size() / 2;
+		size_t i = 0;
+		for (; i < len; i++){
+			destFront.push_back(src[i]);
+		}
+		len *= 2;
+		for (; i < len; i++){
+			destBack.push_back(src[i]);
+		}
+		for (; i < src.size(); i++){
+			remainder.push_back(src[i]);
+		}
+}
+
+template <typename Container>
+void FordJohnsonAlgorithm(Container &src)
+{
+	if (src.size() <= 1){
+		return;
+	}
+	Container Front;
+	Container Back;
+	Container Remainder;
+
+	divideIntoThree(src, Front, Back, Remainder);
+	SwapBigger(Front, Back);
+	Copy(Front, src);
+	FordJohnsonAlgorithm(src);
+	SetHaspair(src);
+	openPair(src, Front, Back);
+	size_t i = Remainder.size();
+	while (i--){
+		src.insert(BinarySearch(src, Remainder[i], src.size()),Remainder[i]);
+	}
+}
+
 #endif
--- ex02/main.cpp
+++ ex02/main.cpp
@@ -2,18 +2,30 @@
 #include <stdlib.h>
 int main(int argc, char *argv[]){
 	
-	//vector part
-	std::vector<Unit> arguments;
+	MyList<Unit> ListArg;
+
 	for (int i = 1; i < argc; i++)
 	{
 		int value = std::atoi(argv[i]);
-		arguments.push_back(Unit(value));
+		ListArg.push_back(Unit(value));
 	}
-	VectorSort(arguments);
-	for (std::vector<Unit>::iterator i = arguments.begin(); i != arguments.end(); i++)
-	{
-		std::cout<< *i <<", ";
+	FordJohnsonAlgorithm(ListArg);
+	for(MyList<Unit>::iterator it = ListArg.begin(); it != ListArg.end(); it++){
+		std::cout<< *it <<", ";
 	}
 	std::cout<< std::endl;
 	return 0;
 }
+
+//vector part
+	//std::vector<Unit> VectorArg;
+// for (int i = 1; i < argc; i++)
+	// {
+	// 	int value = std::atoi(argv[i]);
+	// 	VectorArg.push_back(Unit(value));
+	// }
+//FordJohnsonAlgorithm(VectorArg);
+// for (std::vector<Unit>::iterator i = VectorArg.begin(); i != VectorArg.end(); i++)
+	// {
+	// 	std::cout<< *i <<", ";
+	// }
