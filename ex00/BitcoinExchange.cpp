bf8288545ba9a7e2be9078983be4c606be50adc6
Kimura Ayumi <kayumi@c1r14s1.42tokyo.jp>
2023-11-02 15:32:25 +0900

    new

--- ex00/BitcoinExchange.cpp
+++ ex00/BitcoinExchange.cpp
@@ -1,22 +1,5 @@
 #include "BitcoinExchange.hpp"
 
-void removeSpaces(std::string& str)
-{
-    std::string::iterator position = std::remove(str.begin(), str.end(), ' ');
-    str.erase(position, str.end());
-}
-
-bool FindMinus(std::string &str)
-{
-	std::string::iterator position = std::find(str.begin(), str.end(), '-');
-	if (position != str.end())
-	{
-		return false;//0
-	}
-	else
-		return true;//1
-}
-
 bool CheckDecimals(std::string &str)
 {
 		std::string::iterator position = std::find(str.begin(), str.end(), '.');
@@ -51,36 +34,46 @@ std::map<std::string, float> CsvData(std::ifstream &file)
 	return(Array);
 }
 
+
 void IntPart(std::string &str, std::map<std::string, float> CsvArray, std::string data)
 {
-	std::map<std::string, float>::iterator find = CsvArray.find(str);//実際のデータの情報
-	if (find != CsvArray.end())//キーがある場合。
-		{
-			long num = atol(data.c_str());
-			float n = CsvArray[str] * num;
-			if (num >= 1000)
-				std::cout << "Error: too large a number. "<< std::endl;
-			else if (num < 0)
-				std::cout << "Error: not a positive number." << std::endl;
-			else
-				std::cout << str << " => " << num << " = " << n << std::endl;
-		}
+    std::map<std::string, float>::iterator find = CsvArray.upper_bound(str);
+    if (find == CsvArray.begin()) {
+        std::cout << "Error: no data." << std::endl;
+		return ;
+    }
+    else {
+        --find;
+    }
+    long num = atol(data.c_str());
+    float n = find->second * num;
+    if (num >= 1000)
+        std::cout << "Error: too large a number." << std::endl;
+    else if (num < 0)
+        std::cout << "Error: not a positive number." << std::endl;
+    else
+        std::cout << str<< " ("<< find->first <<")" << " => " << num << " = " << n << std::endl;
 }
 
 void FloatPart(std::string &str, std::map<std::string, float> CsvArray, std::string data)
 {
-	std::map<std::string, float>::iterator find = CsvArray.find(str);//実際のデータの情報
-	if (find != CsvArray.end())//キーがある場合。
-		{
-			float num = atof(data.c_str());
-			float d = CsvArray[str] * num;
-			if (num >= 1000)
-				std::cout << "Error: too large a number. "<< std::endl;
-			else if (num <= 0)
-				std::cout << "Error: not a positive number." << std::endl;
-			else
-				std::cout << str << " => " << num << " = " << d << std::endl;
-		}
+    std::map<std::string, float>::iterator find = CsvArray.upper_bound(str);
+    if (find == CsvArray.begin()) {
+		if (find->first != str)
+        	std::cout << "Error: no data." << std::endl;
+		return ;
+    }
+    else {
+        --find;
+    }
+    float num = atof(data.c_str());
+    float n = find->second * num;
+    if (num >= 1000)
+        std::cout << "Error: too large a number." << std::endl;
+    else if (num < 0)
+        std::cout << "Error: not a positive number." << std::endl;
+    else
+        std::cout << str<< " ("<< find->first <<")" << " => " << num << " = " << n << std::endl;
 }
 
 //検索するデータ
@@ -99,19 +92,18 @@ void inputData(std::ifstream &file, std::map<std::string, float> CsvArray)
 	while(getline(file, str))
 	{
 		std::stringstream ss;//初期化忘れずに
+		std::string buff1;
+		std::string buff2;
 		ss << str;
-		getline(ss, str, '|');
-		getline(ss, data, '\n');
-		removeSpaces(str);
+		ss >> str >> buff1 >> data >> buff2;
+		if (buff1 != "|" || buff2.length()){
+			std::cout << "Error: bad input => " << str << std::endl;
+			continue;	
+		}
 		if (CheckDecimals(data) == false)//少数じゃない
 			IntPart(str, CsvArray, data);
 		else
 			FloatPart(str, CsvArray, data);
-		if (CsvArray.find(str) == CsvArray.end())//キーが無かった場合はここに来る
-		{
-			std::cout << "Error: bad input => " << str << std::endl;
-			continue;
-		}
 	}
 	file.close();
 }
